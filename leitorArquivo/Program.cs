534e715984d512e76f559b4827cf3fc95f47dd1f
Alan Santana <72580528+gankun26@users.noreply.github.com>
2022-01-24 16:37:32 -0300

    lendo arquivo

---
+++ leitorArquivo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace leitorArquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //busca o arquivo na pasta selecionada
+                const string filePath = @"C:\AquivoText\Arquivo1.txt";
+
+                using var file = new StreamReader(filePath);
+
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+                file.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+            }
+    
+        }
+    }
+}
